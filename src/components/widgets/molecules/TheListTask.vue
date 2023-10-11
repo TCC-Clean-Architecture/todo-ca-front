1e85ed86448c98bc6d12f16875ed468afeae3b02
Luan Pessoa <devluan.pessoa@gmail.com>
2023-10-10 23:50:16 -0300

    ✨ feat(the-list, the-list-task): create list and list-task components

---
+++ src/components/widgets/molecules/TheListTask.vue
@@ -0,0 +1,160 @@
+<template>
+	<div class="list-task">
+		<div role="button" class="list-task__handle"></div>
+		<div class="list-task__content">
+			<h3 class="list-task__name">{{ name }}</h3>
+			<p class="list-task__description">
+				{{ description }}
+			</p>
+			<span class="list-task__status" :class="[`list-task__status--${status}`]">
+				{{ status }}
+			</span>
+		</div>
+		<div class="list-task__actions">
+			<button class="list-task__action"></button>
+			<button class="list-task__action"></button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+/* -- Props -- */
+
+interface Props {
+	name?: string;
+	description?: string;
+	status?: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style lang="scss" scoped>
+.list-task {
+	position: relative;
+
+	display: grid;
+	grid-template-columns: 2.5rem 1fr 1.5rem;
+
+	min-height: 13rem;
+	padding: 0.75rem;
+
+	background-color: var(--clr-bg-soft);
+	box-shadow: $elevation-2;
+	border-radius: 0.375rem;
+
+	&__content {
+		display: flex;
+		flex-direction: column;
+		margin-block: 1rem 0.5rem;
+		margin-inline-start: 1rem;
+	}
+
+	&__name {
+		color: var(--clr-primary);
+		font-size: 1.125rem;
+		font-weight: 700;
+
+		margin-block-end: 0.5rem;
+	}
+
+	&__description {
+		color: var(--clr-primary-lighter);
+		font-size: 1rem;
+		font-weight: 400;
+	}
+
+	&__status {
+		margin-block-start: auto;
+		align-self: flex-end;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		height: 28px;
+		padding-inline: 1.25rem;
+
+		font-size: 0.75rem;
+		font-weight: 600;
+		color: var(--clr-text-inverse-1);
+
+		background-color: var(--clr-primary);
+		border-radius: 0.625rem;
+
+		&--done {
+			background-color: var(--clr-success);
+		}
+		&--inprogress {
+			background-color: var(--clr-warning);
+		}
+		&--todo {
+			background-color: var(--clr-info);
+		}
+		&--unknown {
+			background-color: var(--clr-mute);
+			color: var(--clr-mute-darker);
+		}
+	}
+
+	&__handle {
+		background-color: var(--clr-bg-soft-up);
+		color: var(--clr-secondary);
+
+		border-radius: 0.25rem;
+
+		cursor: grab;
+
+		transition: background-color 150ms ease-in-out;
+
+		&:hover {
+			background-color: var(--clr-secondary-lightest);
+
+			svg {
+				scale: 1.1;
+			}
+		}
+
+		svg {
+			width: 2rem;
+			height: 2rem;
+
+			transition: scale 150ms ease-in-out;
+		}
+	}
+
+	&__actions {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+	}
+
+	&__action {
+		width: 1.5rem;
+		height: 1.5rem;
+		padding: 0.125rem;
+
+		color: var(--clr-secondary);
+
+		background-color: var(--clr-bg-soft-up);
+		border-radius: 0.5rem;
+
+		transition: background-color 150ms ease-in-out;
+
+		&:hover {
+			background-color: var(--clr-secondary-lightest);
+
+			svg {
+				scale: 1.1;
+			}
+		}
+
+		svg {
+			transition: scale 150ms ease-in-out;
+
+			width: 100%;
+			height: 100%;
+		}
+	}
+}
+</style>
---
+++ src/components/widgets/organisms/TheList.vue
@@ -0,0 +1,123 @@
+<template>
+	<section class="list">
+		<Draggable
+			tag="ol"
+			class="list__wrapper"
+			handle=".list-task__handle"
+			v-model="tasks"
+			group="tasks"
+			item-key="id"
+			:animation="200"
+		>
+			<template #item="{ element }">
+				<li class="list__task">
+					<TheListTask v-bind="element" />
+				</li>
+			</template>
+			<template #footer>
+				<button class="list__new-task"></button>
+			</template>
+		</Draggable>
+	</section>
+</template>
+
+<script lang="ts" setup>
+import TheListTask from '@/components/widgets/molecules/TheListTask.vue';
+import Draggable from 'vuedraggable';
+
+import { ref } from 'vue';
+
+type ITodo = {
+	id: string;
+	name: string;
+	description: string;
+	status: string;
+};
+
+const tasks = ref<ITodo[]>([
+	{
+		id: '1',
+		name: `Option to "use local/server version" feature`,
+		description: `It usually displays this message when you close an unsaved page when you do it on purpose, and it's getting frustrated to see this every time.`,
+		status: 'Status',
+	},
+	{
+		id: '2',
+		name: `Option to "use local/server version" feature`,
+		description: `It usually displays this message when you close an unsaved page when you do it on purpose, and it's getting frustrated to see this every time.`,
+		status: 'Status',
+	},
+	{
+		id: '3',
+		name: `Option to "use local/server version" feature`,
+		description: `It usually displays this message when you close an unsaved page when you do it on purpose, and it's getting frustrated to see this every time.`,
+		status: 'Status',
+	},
+]);
+</script>
+
+<style lang="scss" scoped>
+.list {
+	$self: &;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+
+	margin: 1.5rem;
+
+	&__title {
+		display: block;
+
+		padding-block: 0.25rem;
+		padding-inline: 1rem;
+
+		color: var(--clr-primary);
+		font-size: 2rem;
+		font-weight: 700;
+
+		background-color: var(--clr-bg-soft);
+		border-radius: 0.5rem;
+	}
+
+	&__wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: 1.25rem;
+
+		background-color: var(--clr-bg-soft);
+		border-radius: 0.5rem;
+		padding: 1.75rem;
+
+		list-style: none;
+	}
+
+	&__new-task {
+		display: grid;
+		place-items: center;
+
+		padding-block: 1rem;
+
+		background-color: var(--clr-bg-soft-up);
+		color: var(--clr-secondary);
+
+		border-radius: 0.25rem;
+
+		transition: background-color 150ms ease-in-out;
+
+		&:hover {
+			background-color: var(--clr-secondary-lightest);
+
+			svg {
+				scale: 1.1;
+			}
+		}
+
+		svg {
+			width: 1.25rem;
+			height: 1.25rem;
+
+			transition: scale 100ms ease-in-out;
+		}
+	}
+}
+</style>
