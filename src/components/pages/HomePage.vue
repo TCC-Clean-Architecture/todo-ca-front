1f3b67330b2e03128c32ccc38e6fde70cdbf035a
Luan Pessoa <devluan.pessoa@gmail.com>
2023-10-10 23:51:27 -0300

    ✨ feat(home-page): add header and list component to home page

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,34 +1,18 @@
 <template>
 	<DashboardLayout>
-		<div class="header">
-			<h1 class="header__title">Dashboard</h1>
-		</div>
-		<section class="dashboard"></section>
 		<template #sidebar>
 			<TheSidebar />
 		</template>
+		<TheHeader />
+		<TheList />
 	</DashboardLayout>
 </template>
 
 <script lang="ts" setup>
 import DashboardLayout from '@/layouts/DashboardLayout.vue';
+import TheHeader from '@/components/widgets/organisms/TheHeader.vue';
 import TheSidebar from '@/components/widgets/organisms/TheSidebar.vue';
+import TheList from '@/components/widgets/organisms/TheList.vue';
 </script>
 
-<style lang="scss" scoped>
-.header {
-	&__title {
-		color: var(--clr-dark);
-	}
-}
-
-.dashboard {
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-	gap: 2rem;
-
-	background-color: var(--clr-bg-elv-down);
-	border-radius: 0.25rem;
-}
-</style>
+<style lang="scss" scoped></style>
