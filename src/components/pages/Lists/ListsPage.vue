d23262fe45ab7b02fe3a95fa1070d9014a44cebd
Luan Pessoa <devluan.pessoa@gmail.com>
2023-10-15 18:22:54 -0300

    ✨ feat(list-page): set the-header from lists-page to list-page

--- src/components/pages/Lists/ListsPage.vue
+++ src/components/pages/Lists/ListsPage.vue
@@ -3,14 +3,12 @@
 		<template #sidebar>
 			<TheSidebar />
 		</template>
-		<TheHeader />
 		<slot></slot>
 	</DashboardLayout>
 </template>
 
 <script lang="ts" setup>
 import DashboardLayout from '@/layouts/DashboardLayout.vue';
-import TheHeader from '@/components/widgets/organisms/TheHeader.vue';
 import TheSidebar from '@/components/widgets/organisms/TheSidebar.vue';
 </script>
 
